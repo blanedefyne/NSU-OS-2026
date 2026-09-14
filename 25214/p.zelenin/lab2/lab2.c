1f323eafe0de41347e92dd8f6946bea9b72a44d3
Pavel Zelenin <p.zelenin@g.nsu.ru>
2026-09-14 10:27:06 +0700

    ctime() - not ctime_r()

--- 25214/p.zelenin/lab2/lab2.c
+++ 25214/p.zelenin/lab2/lab2.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#define MIN_BUFLEN 26
 
 int main() {
 
-    char buf[MIN_BUFLEN];
     time_t t;
     if ((t = time(0)) == -1) {
         perror("error: could not get the time - ");
@@ -17,7 +15,7 @@ int main() {
         exit(EXIT_FAILURE);
     }    
 
-    char* result = ctime_r(&t, buf, MIN_BUFLEN);
+    char* result = ctime(&t);
     if (result == NULL) {
         perror("ctime_r() func caused an error - ");
         exit(EXIT_FAILURE);
